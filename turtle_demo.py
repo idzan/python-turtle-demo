42e4c2bc0472c93a40539ceaf5af42f9d08932d9
tsmaguc <smagye@gmail.com>
2020-04-08 00:45:54 +0200

    promjenaImena
    Promjena imena i pozicije crtanja

--- turtle_demo.py
+++ turtle_demo.py
@@ -1,21 +1,21 @@
 import turtle
 
-john = turtle.Turtle()
-john.getscreen().bgcolor("#3d3d3d")
-john.penup()
-john.goto((-200,100))
-john.pendown()
+shmaggy = turtle.Turtle()
+shmaggy.getscreen().bgcolor("#3d3d3d")
+shmaggy.penup()
+shmaggy.goto((-100,75))
+shmaggy.pendown()
 
 def stars(turtle, size):
     if size <= 10:
         return
     else:
         turtle.begin_fill()
-        for i in range(5):
+        for i in range(20):
             turtle.forward(size)
             stars(turtle, size/3)
             turtle.left(216)
         turtle.end_fill()
 
-stars(john, 360)
+stars(shmaggy, 360)
 turtle.done()
